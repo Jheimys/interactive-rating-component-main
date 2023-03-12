5698a2a612edd89413d988a26177f24f04bf4210
Jheimys Bassani <jheimysbb@gmail.com>
2023-03-12 13:36:08 -0300

    :buld: docs: Adicionando comentários sobre o código em JS

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-const lis = document.querySelectorAll('li')
-const selectCard = document.querySelectorAll('.card')
-const btnSubmit = document.querySelector('button')
+const lis = document.querySelectorAll('li')              //Seleciona todas as lis
+const selectCard = document.querySelectorAll('.card')    //Seleciona todos os card
+const btnSubmit = document.querySelector('button')       // Seleciona o botão de submit
 
 function rating(element) {
-    const paragraphSpan = document.querySelector('.rating span')
-    paragraphSpan.innerHTML = element
+    //Seleciona o span dentro da ta de paragrafo
+    const paragraphSpan = document.querySelector('.rating span') 
+    paragraphSpan.innerHTML = element   //adiciona  dentro do span a nota selecionada
 }
 
 for(let i = 0; i < lis.length; i++) {
-    const selectLi = lis[i]
-    selectLi.addEventListener('click', () => {
-        selectLi.classList.toggle('newLi')
-        const selectNote = selectLi.textContent
-        rating(selectNote)
+    const selectLi = lis[i]                      //lista a li de acordo com a posição
+    selectLi.addEventListener('click', () => {   // adiciona o evento de click na li 
+        selectLi.classList.toggle('newLi')       //adiciona ou remove a classe newLi
+        const selectNote = selectLi.textContent  //imprime o conteúdo da li clicada
+        rating(selectNote)                       //chama a função rating
     })
 }
 
-btnSubmit.addEventListener('click', () => {
-    selectCard[0].classList.add('hidden')
-    selectCard[1].classList.remove('hidden')
+btnSubmit.addEventListener('click', () => {   // evento de click no botão de submit
+    selectCard[0].classList.add('hidden')     // adiciona a classe hidden na 1°div com a classe card
+    selectCard[1].classList.remove('hidden')  // remove a classe hidden na 2°div com a classe card
 })
 
 
